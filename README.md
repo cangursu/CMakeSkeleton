64f99c5854c34b394f319bcf970ef138ad089b81
Can Gursu <cangursu@yahoo.com>
2020-12-09 12:24:28 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CMakeSkeleton
+
+A primitive CmakeList.txt file to start a new project from scratch.
+
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) 
+{
+    std::cout << "CMakeSkeleton project" << '\n'; 
+    return 0;
+}

1c1d41a51c7b0cac6aa725e00dfd41d08d7029aa
cangursu <cangursu@yahoo.com>
2024-03-28 07:22:44 +0000

    - OUTPUT_DIRECTORY dir added - Sample launch.json, tasks.json added for VSCode - Warning flags enhanced

--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -3,9 +3,9 @@
 #include <iostream>
 
 
-int main(int argc, const char * argv[])
+int main(int /*argc*/, const char * /*argv*/[])
 {
     std::cout << "Hello  " << dummy() << std::endl;
-    
+
     return 0;
 }
